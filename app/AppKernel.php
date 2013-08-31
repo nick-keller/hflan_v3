8bc70fdb34968ff354bb6cf87e9ed31d2c0b7956
nick-keller <nicolas.keller@vimies.com>
2013-08-31 20:29:33 +0200

    Included Doctrine extensions to project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new hflan\BlogBundle\hflanBlogBundle(),
             new hflan\UserBundle\hflanUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
