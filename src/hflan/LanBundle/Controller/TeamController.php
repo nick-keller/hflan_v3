3e6cbccc6b58518916e10da6b0d0ea0d6e4ccabe
Benjamin Mousseau <benjamin.mousseau@gmail.com>
2015-04-01 12:00:38 +0200

    stripe pret
    sana upgrade de l'user

--- src/hflan/LanBundle/Controller/TeamController.php
+++ src/hflan/LanBundle/Controller/TeamController.php
@@ -150,6 +150,29 @@ class TeamController extends Controller
             $this->em->flush();
         }
 
+
+        if ($request->isMethod('POST')) {
+            // Set your secret key: remember to change this to your live secret key in production
+            // See your keys here https://dashboard.stripe.com/account/apikeys
+            \Stripe\Stripe::setApiKey("sk_test_i7CdVIOaXs3fb1p23ZKnb7Wp");
+
+            // Get the credit card details submitted by the form
+            $token = $request->request->get('stripeToken');
+
+            // Create the charge on Stripe's servers - this will charge the user's card
+            try {
+                $charge = \Stripe\Charge::create(array(
+                    "amount" => $team->getTournament()->getTotalPrice() * 100, // amount in cents, again
+                    "currency" => "eur",
+                    "source" => $token,
+                    "description" => $team->getName(),
+                    "receipt_email" => $team->getEmail()
+                ));
+            } catch(\Stripe\Error\Card $e) {
+              // The card has been declined
+            }
+        }
+
         return array(
             'team' => $team,
             'tournament' => $team->getTournament(),
