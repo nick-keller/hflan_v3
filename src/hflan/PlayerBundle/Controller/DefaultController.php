ab324fd9a9bdd349dfc044b151f2378e6941d6df
Kévin Ta <kevin.ta@edu.esiee.fr>
2015-11-20 19:18:30 +0100

    Optimization of Player Bundle

--- src/hflan/PlayerBundle/Controller/DefaultController.php
+++ src/hflan/PlayerBundle/Controller/DefaultController.php
@@ -8,109 +8,34 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	$em = $this->getDoctrine()->getManager();
-    	$events = $em->getRepository('hflanLanBundle:Event')->findAll();
-    	$event_id = end($events)->getId();
-    	$is_melee_edition = $em->getRepository('hflanLanBundle:Tournament')->findOneBy(array('event' => $event_id, 'game' => 'League of Legends'));
-    	$has_csgo = $em->getRepository('hflanLanBundle:Tournament')->findOneBy(array('event' => $event_id, 'game' => 'Counter-Strike : Global Offensive'));
+        $em = $this->getDoctrine()->getManager();
+        $next_event = $em->getRepository('hflanLanBundle:Event')->findNextEvent();
 
         return $this->render('hflanPlayerBundle:Default:index.html.twig', array(
-			'event_id' => $event_id,
-			'has_csgo' => $has_csgo,
-			'is_melee_edition' => $is_melee_edition
-		));
+            'next_event' => $next_event
+        ));
     }
 
-    public function sc2Action($id)
+    public function listAction($slug)
     {
-    	$em = $this->getDoctrine()->getManager();
-    	$tournament = $em->getRepository('hflanLanBundle:Tournament')->findOneBy(array('event' => $id, 'gameType' => '1v1'));
-    	
-    	if($tournament == null) return $this->redirectToRoute('hflan_player');
+        $em = $this->getDoctrine()->getManager();
+        $tournament = $em->getRepository('hflanLanBundle:Tournament')->findOneBySlug($slug);
 
-    	$players = $em->getRepository('hflanLanBundle:Team')->findBy(array('tournament' => $tournament->getId(), 'paid' => '1'));
-    	$current = count($players);
+        if($tournament == null) return $this->redirectToRoute('hflan_player');
 
-    	for($i = 0; $i < count($players); $i++)
-    	{
-    		$team = $em->getRepository('hflanLanBundle:Player')->findBy(array('team' => $players[$i]->getId()));
-    		$teams[] = $team;
-    	}
+        $players = $em->getRepository('hflanLanBundle:Team')->findBy(array('tournament' => $tournament->getId(), 'paid' => '1'));
+
+        for($i = 0; $i < count($players); $i++)
+        {
+            $team = $em->getRepository('hflanLanBundle:Player')->findByTeam($players[$i]->getId());
+            $teams[] = $team;
+        }
 
         return $this->render('hflanPlayerBundle:Default:list.html.twig', array(
-        	'tournament' => $tournament,
-			'teams' => $teams,
-			'players' => $players,
-			'current' => count($players)
-		));
-    }
-
-    public function csgoAction($id)
-    {
-    	$em = $this->getDoctrine()->getManager();
-    	$tournament = $em->getRepository('hflanLanBundle:Tournament')->findOneBy(array('event' => $id, 'game' => 'Counter-Strike : Global Offensive'));
-    	
-    	if($tournament == null) return $this->redirectToRoute('hflan_player');
-
-    	$players = $em->getRepository('hflanLanBundle:Team')->findBy(array('tournament' => $tournament->getId(), 'paid' => '1'));
-
-    	for($i = 0; $i < count($players); $i++)
-    	{
-    		$team = $em->getRepository('hflanLanBundle:Player')->findBy(array('team' => $players[$i]->getId()));
-    		$teams[] = $team;
-    	}
-    	
-        return $this->render('hflanPlayerBundle:Default:list.html.twig', array(
-        	'tournament' => $tournament,
-			'teams' => $teams,
-			'players' => $players,
-			'current' => count($players)
-		));
-    }
-
-    public function lolAction($id)
-    {
-    	$em = $this->getDoctrine()->getManager();
-    	$tournament = $em->getRepository('hflanLanBundle:Tournament')->findOneBy(array('event' => $id, 'game' => 'League of Legends'));
-    	
-    	if($tournament == null) return $this->redirectToRoute('hflan_player');
-
-    	$players = $em->getRepository('hflanLanBundle:Team')->findBy(array('tournament' => $tournament->getId(), 'paid' => '1'));
-
-    	for($i = 0; $i < count($players); $i++)
-    	{
-    		$team = $em->getRepository('hflanLanBundle:Player')->findBy(array('team' => $players[$i]->getId()));
-    		$teams[] = $team;
-    	}
-
-        return $this->render('hflanPlayerBundle:Default:list.html.twig', array(
-        	'tournament' => $tournament,
-			'teams' => $teams,
-			'players' => $players,
-			'current' => count($players)
-		));
-    }
-
-    public function ssbmAction($id)
-    {
-    	$em = $this->getDoctrine()->getManager();
-    	$tournament = $em->getRepository('hflanLanBundle:Tournament')->findOneBy(array('event' => $id, 'game' => 'Super Smash Bros. Melee'));
-    	
-    	if($tournament == null) return $this->redirectToRoute('hflan_player');
-
-    	$players = $em->getRepository('hflanLanBundle:Team')->findBy(array('tournament' => $tournament->getId(), 'paid' => '1'));
-
-    	for($i = 0; $i < count($players); $i++)
-    	{
-    		$team = $em->getRepository('hflanLanBundle:Player')->findBy(array('team' => $players[$i]->getId()));
-    		$teams[] = $team;
-    	}
-
-        return $this->render('hflanPlayerBundle:Default:list.html.twig', array(
-        	'tournament' => $tournament,
-			'teams' => $teams,
-			'players' => $players,
-			'current' => count($players)
-		));
+            'tournament' => $tournament,
+            'teams' => $teams,
+            'players' => $players,
+            'current' => count($players)
+        ));
     }
 }
