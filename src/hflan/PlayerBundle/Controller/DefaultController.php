4ab8b1376b811515349fcfac1797fb5185d368a4
Kévin Ta <kevin.ta@edu.esiee.fr>
2016-03-06 18:14:24 +0100

    Generate players lists for the blog

--- src/hflan/PlayerBundle/Controller/DefaultController.php
+++ src/hflan/PlayerBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace hflan\PlayerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use hflan\BlogBundle\Entity\Article;
+use \DateTime;
 
 class DefaultController extends Controller
 {
@@ -37,5 +39,49 @@ class DefaultController extends Controller
             'players' => $players,
             'current' => count($players)
         ));
+    }
+
+    public function generateAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->get('security.context')->getToken()->getUser();
+        $next_event = $em->getRepository('hflanLanBundle:Event')->findNextEvent();
+
+        if($next_event == null) return $this->redirectToRoute('hflan_blog_admin');
+
+        $tournaments = $em->getRepository('hflanLanBundle:Tournament')->findByEvent($next_event);
+
+        foreach($tournaments as $tournament)
+        {
+            if($tournament->getGameType() != 'Tous jeux' && $tournament->getGameType() != 'Spectateur')
+            {
+                $players = $em->getRepository('hflanLanBundle:Team')->findBy(array('tournament' => $tournament->getId(), 'paid' => '1'));
+
+                for($i = 0; $i < count($players); $i++)
+                {
+                    $team = $em->getRepository('hflanLanBundle:Player')->findByTeam($players[$i]->getId());
+                    $teams[] = $team;
+                }
+
+                $article = new Article();
+                $article->setTitle($next_event->getName().' - Liste des joueurs '.$tournament->getGame());
+                $article->setAuthor($user);
+                $article->setCreatedAt(new DateTime());
+                $article->setPublished(1);
+                $article->setLang('fr');
+                $article->setContent(
+                    $this->renderView('hflanPlayerBundle:Default:blog.html.twig', array(
+                    'tournament' => $tournament,
+                    'teams' => $players ? $teams : null,
+                    'players' => $players,
+                    'current' => count($players)
+                    ))
+                );
+                $em->persist($article);
+                $em->flush();
+            }
+        }
+
+        return $this->redirectToRoute('hflan_blog_admin');
     }
 }
