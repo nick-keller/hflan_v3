39b494471a451c9210aeef664f47660e8f47138b
Kévin Ta <kevin.ta@edu.esiee.fr>
2016-03-28 11:04:39 +0200

    Fixed FB API using a permanent access_token hack

--- src/hflan/PhotosBundle/Resources/public/js/albums.js
+++ src/hflan/PhotosBundle/Resources/public/js/albums.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.getJSON('https://graph.facebook.com/hf.lan.esiee?fields=albums.fields(name,type,count,id,photos.limit(3).fields(picture))', function(data){
+    $.getJSON('https://graph.facebook.com/hf.lan.esiee?fields=albums.fields(name,type,count,id,photos.limit(3).fields(picture))&access_token=CAAOwtDcpVY8BAEYZBmZBG7UYOPlACddJSquCsDrvoHwnawgZAlysweLsPbBjbLH0blvHup8MDj2jTvuV7Uv9UFaTHhFu45JGRg5FHEsGFZCngtt44qdZC8NwLwLoBQoybQDueQfSow5qx3XtaNwU0D7obRs0ZArPAoWgLMhgtdIZBZASDcljmbOJYXyNZB9EsvRYIeAsGBTLBRwZDZD', function(data){
         $('#photo-albums').html('');
         $.each(data.albums.data, function(i, e){
             if(e.type == 'normal' && e.count > 20){
--- src/hflan/PhotosBundle/Resources/public/js/photos.js
+++ src/hflan/PhotosBundle/Resources/public/js/photos.js
@@ -1,7 +1,7 @@
 $(function(){
     var id = $('#photo-gallery').attr('data-id');
 
-    $.getJSON('https://graph.facebook.com/'+id+'?fields=from,photos.limit(300).fields(source,picture),name', function(data){
+    $.getJSON('https://graph.facebook.com/'+id+'?fields=from,photos.limit(300).fields(source,picture),name&access_token=CAAOwtDcpVY8BAEYZBmZBG7UYOPlACddJSquCsDrvoHwnawgZAlysweLsPbBjbLH0blvHup8MDj2jTvuV7Uv9UFaTHhFu45JGRg5FHEsGFZCngtt44qdZC8NwLwLoBQoybQDueQfSow5qx3XtaNwU0D7obRs0ZArPAoWgLMhgtdIZBZASDcljmbOJYXyNZB9EsvRYIeAsGBTLBRwZDZD', function(data){
         if(data.error != undefined || data.from == undefined || data.from.id != 236675399770934){
             $('#album-title').html('Erreur');
             $('#photo-gallery').html("<p>Cet album photo n'existe pas...</p>");
