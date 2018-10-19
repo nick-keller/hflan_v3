2bec7d3c8593d4852ed822d2a49a29d157359753
kevin-ta <ta_kevin@hotmail.fr>
2018-10-19 08:59:47 +0200

    Changed access token for FB albums

--- src/hflan/PhotosBundle/Resources/public/js/albums.js
+++ src/hflan/PhotosBundle/Resources/public/js/albums.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.getJSON('https://graph.facebook.com/hf.lan.esiee?fields=albums.fields(name,type,count,id,photos.limit(3).fields(picture))&access_token=CAAOwtDcpVY8BAEYZBmZBG7UYOPlACddJSquCsDrvoHwnawgZAlysweLsPbBjbLH0blvHup8MDj2jTvuV7Uv9UFaTHhFu45JGRg5FHEsGFZCngtt44qdZC8NwLwLoBQoybQDueQfSow5qx3XtaNwU0D7obRs0ZArPAoWgLMhgtdIZBZASDcljmbOJYXyNZB9EsvRYIeAsGBTLBRwZDZD', function(data){
+    $.getJSON('https://graph.facebook.com/hf.lan.esiee?fields=albums.fields(name,type,count,id,photos.limit(3).fields(picture))&access_token=EAAOwtDcpVY8BACEN3MaijhLdD0BZAwZCl02zpo1vKsZCPgatQUUEfqtSIv35TD8zWNaUz2hjJXJwUhYEZBZCIO5ROFFQZBk8oNQactVbAdR4ebK82RAfp8XBZCMH3uiLZBpeA0jV5c4k3iP3rlUD0ECAfWap6HTCaGS9oXtJLeaiT68vwTszAZAl35b6QGT3X42oJAYPB7bZBJ0AZDZD', function(data){
         $('#photo-albums').html('');
         $.each(data.albums.data, function(i, e){
             if(e.type == 'normal' && e.count > 20){
--- www/js/photos.js
+++ www/js/photos.js
@@ -1,7 +1,7 @@
 $(function(){
     var id = $('#photo-gallery').attr('data-id');
 
-    $.getJSON('https://graph.facebook.com/'+id+'?fields=from,photos.limit(300).fields(source,picture),name', function(data){
+    $.getJSON('https://graph.facebook.com/'+id+'?fields=from,photos.limit(300).fields(source,picture),name&access_token=EAAOwtDcpVY8BACEN3MaijhLdD0BZAwZCl02zpo1vKsZCPgatQUUEfqtSIv35TD8zWNaUz2hjJXJwUhYEZBZCIO5ROFFQZBk8oNQactVbAdR4ebK82RAfp8XBZCMH3uiLZBpeA0jV5c4k3iP3rlUD0ECAfWap6HTCaGS9oXtJLeaiT68vwTszAZAl35b6QGT3X42oJAYPB7bZBJ0AZDZD', function(data){
         if(data.error != undefined || data.from == undefined || data.from.id != 236675399770934){
             $('#album-title').html('Erreur');
             $('#photo-gallery').html("<p>Cet album photo n'existe pas...</p>");
